e87814c367127e45a8dcd331a42a0e80b6eb1341
Nathan Love <nlovephd@gmail.com>
2023-06-22 14:41:45 -0400

    add client repo

---
+++ FlaskServer/app.py
@@ -0,0 +1,27 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+from flask import Flask
+
+load_dotenv()
+
+app = Flask(__name__)
+url = os.environ.get("DATABASE_URL")
+connection = psycopg2.connect(dbname="tasktracker", user="nathanlove")
+
+@app.route("/test")
+def helloworld():
+    return "testing testing 1 2 3"
+
+@app.route("/pg-test")
+def readAll():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM tasks")
+            results = cursor.fetchall()
+            for row in results:
+                print(row)
+    return "yo"
+
+if __name__ == "__main__":
+    app.run()
