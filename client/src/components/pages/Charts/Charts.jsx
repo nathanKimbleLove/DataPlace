4a633e0d2b9b705deab6c733e51e535c388f6e8f
Katy Atchison <katy.atchison20@gmail.com>
2023-06-23 16:36:43 -0500

    Client redesign, added function for making API calls

--- client/src/components/pages/Charts/Charts.jsx
+++ client/src/components/pages/Charts/Charts.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Charts = () => {
-    return (<p>Charts</p>)
+    return (
+        <>
+            <h4>New Chart</h4>
+            <p>Welcome to our chart generation page! Let's get started.</p>
+            <p>Select the <b>type</b> of visualization you would like to create using the dropdown, and enter a name for your chart (Example: <i>"Average Yearly Air Travel Statistics"</i>).</p>
+            <p>Next, specify the <b>label</b> and <b>units</b> you would like to use for the X and Y axes. Don't worry - you will be able to edit this later.</p>
+            <hr></hr>
+
+
+        </>
+    )
 }
 
 export default Charts;
---
+++ client/src/utils/sendRequest.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+var URL_BACKEND = "http://localhost:5000/";
+
+export async function sendRequest(url, method, body) {
+    const options = {
+        url: `${URL_BACKEND}${url}`,
+        method: method,
+        headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=UTF-8",
+        },
+        body: body || null
+    };
+
+    const response = await axios(options);
+
+    return response;
+}
